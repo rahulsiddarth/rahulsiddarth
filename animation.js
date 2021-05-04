ebf6cb0e4fad2d591e5ca42977aa8b0d8ab61f10
Rahulsiddarth <43289009+rahulsiddarth@users.noreply.github.com>
2021-05-04 17:37:18 +0530

    Create animation.js

---
+++ animation.js
@@ -0,0 +1,3 @@
+var animation = bodymovin.loadAnimation({container: document.getElementById('bloggingAnimation'),
+renderer: 'svg',loop: true,autoplay: true,path: 'https://github.com/rahulsiddarth/rahulsiddarth/blob/main/Blogging.json'
+})
