20487485c742a7f3868a839226c833cb3b6ae7fb
Rahulsiddarth <43289009+rahulsiddarth@users.noreply.github.com>
2021-05-04 17:44:31 +0530

    Delete animation.js

--- animation.js
+++
@@ -1,3 +0,0 @@
-var animation = bodymovin.loadAnimation({container: document.getElementById('bloggingAnimation'),
-renderer: 'svg',loop: true,autoplay: true,path: 'https://github.com/rahulsiddarth/rahulsiddarth/blob/main/Blogging.json'
-})
